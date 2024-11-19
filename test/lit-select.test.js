d09343986f92d7a188e159bbd5ec2de55958c330
andreluiz.dsn.pack <andreluiz.dsn.pack@alterdata.com.br>
2024-11-19 09:03:09 -0300

    inicio dos testes

--- test/lit-select.test.js
+++ test/lit-select.test.js
@@ -1,8 +1,17 @@
+/* eslint-disable no-unused-expressions */
 import { html } from 'lit'
 import { fixture, expect } from '@open-wc/testing'
 
 import '../lit-select.js'
 
 describe('LitSelect', () => {
-  
+  let el
+  const mockOptions = ['Aracaju', 'Belém', 'Curitiba', 'Duque de Caxias']
+  beforeEach(async () => {
+    el = await fixture(html`<lit-select .options=${mockOptions}></lit-select>`)
+  })
+
+  it('should exist', () => {
+    expect(el).to.exist
+  })
 })
